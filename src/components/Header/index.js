44b060a59f57379290213cfb4c3dbd9d414eb327
Travis Johnston <tjohnst1@gmail.com>
2018-04-01 15:51:07 -0700

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import monogram from '../../../public/static/img/monogram.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={monogram} />
+    </header>
+  )
+}
+export default Header
