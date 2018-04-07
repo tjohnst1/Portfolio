c43fe701aa540f86ccbea5dc9a65b4f94a54e86a
Travis Johnston <tjohnst1@gmail.com>
2018-04-07 15:00:44 -0700

    update hero component with link; delete 'page-2' layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,11 @@ import React from 'react';
 import '../scss/hero.scss';
 
 const Hero = () => (
-  <section className="hero">
-    <h2 className="hero__text">Hello! I&rsquo;m Travis Johnston, a <a className="link" href="https://github.com/tjohnst1">web developer</a> based in Portland,&nbsp;Oregon.</h2>
-  </section>
+  <div className="container">
+    <section className="hero">
+      <h2 className="hero__text">Hello! I&rsquo;m Travis Johnston, a <a className="link" href="https://github.com/tjohnst1">web developer</a> based in Portland,&nbsp;Oregon.</h2>
+    </section>
+  </div>
 )
 
 export default Hero;
