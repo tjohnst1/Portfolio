ef6d1355ebba8ee9675a3a340c173bac1b35d9a4
Travis Johnston <tjohnst1@gmail.com>
2018-04-29 14:41:26 -0700

    install json gatsby transformer + source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Starter Blog',
+    author: 'Travis Johnston',
+    description: 'A starter blog demonstrating what Gatsby can do.',
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem' ,
+      options: {
+        name: 'data' ,
+        path: `${__dirname}/src/data`,
+      },
+    },
+    'gatsby-transformer-json',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
   ],
