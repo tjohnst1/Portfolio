c5b43acf0709fdcaee0e2ca8287fe8f3b62c9683
Travis Johnston <tjohnst1@gmail.com>
2018-06-20 21:30:24 -0700

    update footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import '../scss/footer.scss'
 
 const Footer = () => {
   return (
-    <footer id="contact" className="footer container">
-      <h3 className="footer__subhead">Looking to start a project? <a href="mailto:tjohnst1@gmail.com">Let&rsquo;s talk.</a></h3>
-      <ul className="footer__links">
-        <li><a href="https://github.com/tjohnst1">Github</a></li>
-        <li><a href="https://www.linkedin.com/in/tjohnst1/">Linked In</a></li>
-        <li><a href="mailto:tjohnst1@gmail.com">Email</a></li>
-      </ul>
+    <footer id="contact" className="footer">
+      <div className="container">
+        <h3 className="footer__subhead">Looking to start a project? Let&rsquo;s talk.</h3>
+        <ul className="footer__links">
+          <li><a href="https://github.com/tjohnst1">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/tjohnst1/">Linked In</a></li>
+          <li><a href="mailto:tjohnst1@gmail.com">Email</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
