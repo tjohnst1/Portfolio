ad898690254d8bb0ca8682e01846908594a8d83c
Travis Johnston <tjohnst1@gmail.com>
2018-06-20 21:33:52 -0700

    update project page styling

--- src/templates/projects.js
+++ src/templates/projects.js
@@ -4,23 +4,24 @@ import '../scss/project.scss';
 export default ({ data }) => {
   const project = data.projectsJson;
   return (
-    <div className="container">
-      <img src="#" />
-      <section className="project">
-        <h1 className="project__title">{ project.title }</h1>
-        <p className="project__subhead">{ project.category }</p>
-        <p className="project__summary">{ project.summary }</p>
-
+    <div className="project">
+      <div className="container">
+        <div className="project__description">
+          <h1 className="project__title">{ project.name }</h1>
+          <p className="project__subhead">{ project.category }</p>
+          <p className="project__summary" style={{whiteSpace: 'pre-wrap'}}>{ project.summary }</p>
+        </div>
         <div className="project__buttons">
           <a className="button" href={ project.link }>View Project</a>
           <a className="button" href={ project.repository }>Github</a>
         </div>
-
-        <div className="project__metadata">
+      </div>
+      <div className="project__metadata">
+        <div className="container">
           <h3>Programming Languages and/or Libraries Used:</h3>
           <p>{ project.technologies.join(", ") }</p>
         </div>
-      </section>
+      </div>
     </div>
   )
 }
